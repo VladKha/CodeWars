a952381cc66a9aa2a404097392169b80580e6a87
vladkha <vladislavkhaitov@gmail.com>
2017-06-10 09:45:33 +0300

    Add 'Welcome!'

---
+++ 8 kyu/Welcome!/solve.py
@@ -0,0 +1,23 @@
+GREETINGS = {
+    'english': 'Welcome',
+    'czech': 'Vitejte',
+    'danish': 'Velkomst',
+    'dutch': 'Welkom',
+    'estonian': 'Tere tulemast',
+    'finnish': 'Tervetuloa',
+    'flemish': 'Welgekomen',
+    'french': 'Bienvenue',
+    'german': 'Willkommen',
+    'irish': 'Failte',
+    'italian': 'Benvenuto',
+    'latvian': 'Gaidits',
+    'lithuanian': 'Laukiamas',
+    'polish': 'Witamy',
+    'spanish': 'Bienvenido',
+    'swedish': 'Valkommen',
+    'welsh': 'Croeso'
+}
+
+
+def greet(language):
+    return GREETINGS.get(language, 'english')
