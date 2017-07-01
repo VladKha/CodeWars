1c70e97c224730cdcaf3cfb7088f587a7e7d073f
vladkha <vladislavkhaitov@gmail.com>
2017-07-01 13:46:37 +0300

    Add 'Check for prime numbers'

---
+++ 7 kyu/Check for prime numbers/solve.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
