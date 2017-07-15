dbe57b8869ed8ee2aa2cf2a76cf343cefb3d11eb
vladkha <vladislavkhaitov@gmail.com>
2017-07-15 12:24:52 +0300

    Add 'NATO Phonetic Alphabet'

---
+++ 7 kyu/NATO Phonetic Alphabet/solve.py
@@ -0,0 +1,15 @@
+letters = {
+    "A": "Alpha", "B": "Bravo", "C": "Charlie",
+    "D": "Delta", "E": "Echo", "F": "Foxtrot",
+    "G": "Golf", "H": "Hotel", "I": "India",
+    "J": "Juliett", "K": "Kilo", "L": "Lima",
+    "M": "Mike", "N": "November", "O": "Oscar",
+    "P": "Papa", "Q": "Quebec", "R": "Romeo",
+    "S": "Sierra", "T": "Tango", "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+}
+
+
+def nato(word):
+    return ' '.join(letters[c.upper()] for c in word)
