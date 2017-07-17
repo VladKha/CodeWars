4f7cab5c6ba06d93e14ea572250c69d34b6eae8a
vladkha <vladislavkhaitov@gmail.com>
2017-07-17 09:30:20 +0300

    Add 'Did I Finish my Sudoku?'

--- 5 kyu/Did I Finish my Sudoku?/solve.py
+++ 5 kyu/Did I Finish my Sudoku?/solve.py
@@ -6,9 +6,9 @@ def done_or_not(board):
 
     rows = [board[i, :] for i in range(9)]
     cols = [board[:, j] for j in range(9)]
-    sqrs = [board[i:i + 3, j:j + 3].flatten() for i in [0, 3, 6] for j in [0, 3, 6]]
+    squares = [board[i:i + 3, j:j + 3].flatten() for i in [0, 3, 6] for j in [0, 3, 6]]
 
-    for view in np.vstack((rows, cols, sqrs)):
+    for view in np.vstack((rows, cols, squares)):
         if len(np.unique(view)) != 9:
             return 'Try again!'
 
