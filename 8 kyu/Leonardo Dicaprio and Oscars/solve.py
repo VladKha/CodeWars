95110a128674318d1fe7c2ce53ffd04c3c274129
vladkha <vladislavkhaitov@gmail.com>
2017-07-23 11:38:02 +0300

    Add 'Leonardo Dicaprio and Oscars'

---
+++ 8 kyu/Leonardo Dicaprio and Oscars/solve.py
@@ -0,0 +1,8 @@
+def leo(oscar):
+    if oscar == 88:
+        return "Leo finally won the oscar! Leo is happy"
+    elif oscar == 86:
+        return "Not even for Wolf of wallstreet?!"
+    elif oscar < 88:
+        return "When will you give Leo an Oscar?"
+    return "Leo got one already!"
