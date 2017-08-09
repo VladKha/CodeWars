bc542112e172d3c1c770588469fa059aa20e3824
vladkha <vladislavkhaitov@gmail.com>
2017-08-09 12:22:45 +0300

    Improve readability of solution

--- 4 kyu/Pick peaks/solve.py
+++ 4 kyu/Pick peaks/solve.py
@@ -4,7 +4,7 @@ def pick_peaks(arr):
     for i in range(1, len(arr) - 1):
         if arr[i] != arr[pos]:
             pos = i
-        if arr[pos - 1] < arr[pos] and arr[i + 1] < arr[pos] and pos != 0:
+        if pos and arr[pos - 1] < arr[pos] > arr[i + 1]:
             result['pos'].append(pos)
             result['peaks'].append(arr[pos])
     return result
