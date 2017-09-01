4d9a3cba6c97ad6ce75b891e14b5c7289852dd0c
vladkha <vladislavkhaitov@gmail.com>
2017-09-01 15:51:35 +0300

    Add 'Grasshopper - Terminal Game #1'

---
+++ 8 kyu/Grasshopper - Terminal Game #1/solve.py
@@ -0,0 +1,7 @@
+class Hero:
+    def __init__(self, name='Hero'):
+        self.name = name
+        self.position = '00'
+        self.health = 100
+        self.damage = 5
+        self.experience = 0
