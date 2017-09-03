b07f4538b85f836d7fd9622b7b8f0ba4201270b3
vladkha <vladislavkhaitov@gmail.com>
2017-09-03 12:25:35 +0300

    Refactor to python3

--- 8 kyu/Fuel Calculator/solve.py
+++ 8 kyu/Fuel Calculator/solve.py
@@ -1,3 +1,3 @@
-def fuelPrice(litres, pricePerLiter):
+def fuel_price(litres, price_per_liter):
     discount = min(litres // 2 * 5, 25) * 0.01
-    return round(litres * (pricePerLiter - discount), 2)
+    return round(litres * (price_per_liter - discount), 2)
