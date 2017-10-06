7d18e1761d43b279695ef554c78158c4e2cd16dc
vladkha <vladislavkhaitov@gmail.com>
2017-10-06 12:26:56 +0300

    Add 'Alternate capitalization'

---
+++ 7 kyu/Alternate capitalization/solve.py
@@ -0,0 +1,6 @@
+def capitalize(s):
+    result = ['', '']
+    for i,c in enumerate(s):
+        result[0] += c.lower() if i % 2 else c.upper()
+        result[1] += c.upper() if i % 2 else c.lower()
+    return result
