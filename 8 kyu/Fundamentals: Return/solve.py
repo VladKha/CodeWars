bdcd82f71bba8d404bb29350ea5ade6b4b3a05d5
vladkha <vladislavkhaitov@gmail.com>
2017-11-15 15:16:45 +0200

    Add 'Fundamentals: Return'

---
+++ 8 kyu/Fundamentals: Return/solve.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def mod(a, b):
+    return a % b
+
+
+def exponent(a, b):
+    return a ** b
+
+
+def subt(a, b):
+    return a - b
