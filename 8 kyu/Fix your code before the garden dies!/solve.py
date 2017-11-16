96a1aa3e2f7e07ce6c2bc862cb5be46ad69cb160
vladkha <vladislavkhaitov@gmail.com>
2017-11-16 13:26:22 +0200

    Add "Fix your code before the garden dies!"

---
+++ 8 kyu/Fix your code before the garden dies!/solve.py
@@ -0,0 +1,4 @@
+def rain_amount(mm):
+    if mm < 40:
+        return "You need to give your plant " + str(40 - mm) + "mm of water"
+    return "Your plant has had more than enough water for today!"
